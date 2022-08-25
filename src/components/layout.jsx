a79c1294f1d8fe502e917c836c3f156bd447e30b
VladMit1 <mitin1508@gmail.com>
2022-08-25 19:36:58 +0200

    1b Strona główna - Nawigacja i sekcja główna

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,50 +2,36 @@ import { Link, Outlet } from 'react-router-dom';
 
 export const Layout = () => {
    return (
-      <>
-         <section className="nav">
-            <div className="nav__background"></div>
-            <div className="nav__form">
-               <div className="form__auth">
-                  <Link to="/logowanie" className="form__log">
-                     Zaloguj
-                  </Link>
-                  <Link to="/rejestracja" className="form__log">
-                     Register
-                  </Link>
-                  {/*<Link to="/wylogowano" className="form__log">Logout</Link>*/}
-                  <Outlet />
-               </div>
-               <nav className="form__links">
-                  <Link to="/" className="link">
-                     Start
-                  </Link>
-                  <a href="#" className="link">
-                     O co chodzi?
-                  </a>
-                  <a href="#" className="link">
-                     O nas
-                  </a>
-                  <a href="#" className="link">
-                     Fundacja i organizacje
-                  </a>
-                  <a href="#" className="link">
-                     Kontakt
-                  </a>
-               </nav>
-
-               <div className="nav__title">
-                  <div className="">Zacznij pomagać!</div>
-                  <div className="">Oddaj niechciane rzeczy w zaufane ręce</div>
-               </div>
-               <div className="nav__decoration"></div>
-               <div className="nav__buttons">
-                  <Link to="/oddaj-rzeczy" className="btn">Oddaj Rzeczy</Link>
-                  <Link to="/oddaj-rzeczy" className="btn">ZORGANIZUJ<br/> ZBIÓRKĘ</Link>
-               </div>
+      <div className="layout">
+         <nav className="layout__form-log">
+            <div className="log-links">
+               <Link to="/logowanie" className="log-link">
+                  Zaloguj
+               </Link>
+               <Link to="/rejestracja" className="log-link">
+               Załóż konto
+               </Link>
+               {/*<Link to="/wylogowano" className="log-link">Logout</Link>*/}
             </div>
-         </section>
-         <footer>2022</footer>
-      </>
+            <nav className="form__links">
+               <Link to="/" className="link">
+                  Start
+               </Link>
+               <a href="#" className="link">
+                  {/*//scrolls*/}O co chodzi?
+               </a>
+               <a href="#" className="link">
+                  O nas
+               </a>
+               <a href="#" className="link">
+                  Fundacja i organizacje
+               </a>
+               <a href="#" className="link">
+                  Kontakt
+               </a>
+            </nav>
+         </nav>
+         <Outlet />
+      </div>
    );
 };
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
-
    return (
-      <div className=""></div>
+      <div className="home">
+         <div className="home__background"></div>
+         <div className="home__form">
+            <div className="form__title">
+               Zacznij pomagać!<br></br>Oddaj niechciane rzeczy w zaufane ręce
+            </div>
+
+            <div className="form__decoration"></div>
+            <div className="form__buttons">
+               <Link to="/oddaj-rzeczy" className="btn">
+                  ODDAJ <br></br> RZECZY
+               </Link>
+               <Link to="/oddaj-rzeczy" className="btn">
+                  ZORGANIZUJ
+                  <br /> ZBIÓRKĘ
+               </Link>
+            </div>
+         </div>
+      </div>
    );
-}
+};
